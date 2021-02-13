19ee32bf89a42e631ff227dd9a74f83d4330d6e7
CustomizedMe <45433611+CustomizedMe@users.noreply.github.com>
2021-02-13 15:16:00 +0530

    some changes

---
+++ src/Api/LoginApi.js
@@ -0,0 +1,8 @@
+import Axios from "axios";
+
+const BASE_URL = "https://reqres.in/api";
+
+// export function addCustomer(customer) {
+//   const url = +"/users";
+//   return Axios.post(url, customer).then((data) => data.data);
+// }
